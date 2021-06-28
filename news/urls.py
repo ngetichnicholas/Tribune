935696b7d27a09c2c4815e35e546321a83665f17
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-28 23:21:29 +0300

    Add Links

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns =[
     url(r'^$', views.welcome, name = 'welcome'),
     url(r'^today/$', views.news_of_day, name = 'newsToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews') 
 ]
