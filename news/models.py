1adcf2bab9ef7bfcad72067a9648792ac31ad1bc
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-29 15:28:19 +0300

    Testing Editor class

--- news/models.py
+++ news/models.py
@@ -5,3 +5,21 @@ class Editor(models.Model):
   first_name = models.CharField(max_length=300)
   last_name = models.CharField(max_length=300)
   email = models.EmailField()
+
+  def __str__(self):
+    return self.first_name
+
+  class Meta:
+    ordering = ['first_name']
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
