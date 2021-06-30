762d0a42d1f1b99248214d052cf2cd5800881bbd
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-30 13:46:17 +0300

    Upload images

--- news/models.py
+++ news/models.py
@@ -29,6 +29,7 @@ class Article(models.Model):
   editor = models.ForeignKey(Editor)
   tags = models.ManyToManyField(tags)
   pub_date = models.DateTimeField(auto_now_add=True)
+  article_image = models.ImageField(upload_to = 'articles/')
 
   def __str__(self):
     return self.title
