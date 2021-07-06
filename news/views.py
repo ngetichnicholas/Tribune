0536817de493bb79e79cdd7ea7673df8139bf542
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-06 12:14:18 +0300

    Add newsletter form

--- news/views.py
+++ news/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Article
+from django.core.exceptions import ObjectDoesNotExist
+from .forms import NewsLetterForm
+
 
 
 # Create your views here.
@@ -12,7 +15,13 @@ def welcome(request):
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 def past_days_news(request, past_date):
     try:
@@ -45,6 +54,6 @@ def search_results(request):
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
-    except DoesNotExist:
+    except ObjectDoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
